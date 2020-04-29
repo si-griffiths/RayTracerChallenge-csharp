80b67e5591a449eb4417ffa8e0112c9635764638
Simon Griffiths <si_griffiths@yahoo.com>
2020-04-29 14:40:53 +0100

    Constructing and inspecting a 4x4 matrix

---
+++ RayTracer.Tests/features/Matrices.feature
@@ -0,0 +1,16 @@
+﻿Feature: Matrices
+
+Scenario: Constructing and inspecting a 4x4 Matrix
+	Given the following "4x4" matrix m:
+	| 1    | 2    | 3    | 4    |
+	| 1    | 2    | 3    | 4    |
+	| 5.5  | 6.5  | 7.5  | 8.5  |
+	| 9    | 10   | 11   | 12   |
+	| 13.5 | 14.5 | 15.5 | 16.5 |
+	Then m(0, 0) == 1
+	And m(0, 3) == 4
+	And m(1, 0) == 5.5
+	And m(1, 2) == 7.5
+	And m(2, 2) == 11
+	And m(3, 0) == 13.5
+	And m(3, 2) == 15.5
---
+++ RayTracer.Tests/features/MatricesSteps.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using TechTalk.SpecFlow;
+
+namespace RayTracer.Tests.features
+{
+    [Binding]
+    public class MatricesSteps
+    {
+        private rtMatrix4x4 matrix4x4;
+        [Given(@"the following (.*) matrix m:")]
+        public void GivenTheFollowingMatrixM(string p0, Table table)
+        {
+            double[] row1 = Array.ConvertAll(table.Rows[0].Values.ToArray(), double.Parse);
+            double[] row2 = Array.ConvertAll(table.Rows[1].Values.ToArray(), double.Parse);
+            double[] row3 = Array.ConvertAll(table.Rows[2].Values.ToArray(), double.Parse);
+            double[] row4 = Array.ConvertAll(table.Rows[3].Values.ToArray(), double.Parse);
+
+            matrix4x4 = new rtMatrix4x4(row1[0], row1[1], row1[2], row1[3], row2[0], row2[1], row2[2], row2[3], row3[0], row3[1], row3[2], row3[3], row4[0], row4[1], row4[2], row4[3]);
+        }
+        
+        [Then(@"m\((.*), (.*)\) == (.*)")]
+        public void ThenM(int row, int column, double expectedValue)
+        {
+            var value = matrix4x4.GetValue(row, column);
+            Assert.AreEqual(expectedValue, value);
+        }
+
+    }
+}
