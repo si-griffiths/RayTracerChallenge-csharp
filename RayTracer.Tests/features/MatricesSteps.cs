500b0cc8c6003ae9956e48d1e2b27b2b6f43e46f
Simon Griffiths <si_griffiths@yahoo.com>
2020-05-04 16:55:48 +0100

    Added helper method to create 4x4 matrix in the tests

--- RayTracer.Tests/features/MatricesSteps.cs
+++ RayTracer.Tests/features/MatricesSteps.cs
@@ -16,12 +16,7 @@ namespace RayTracer.Tests.features
         [Given(@"the following four by four matrix m:")]
         public void GivenTheFollowingMatrixM(Table table)
         {
-            double[] row1 = Array.ConvertAll(table.Rows[0].Values.ToArray(), double.Parse);
-            double[] row2 = Array.ConvertAll(table.Rows[1].Values.ToArray(), double.Parse);
-            double[] row3 = Array.ConvertAll(table.Rows[2].Values.ToArray(), double.Parse);
-            double[] row4 = Array.ConvertAll(table.Rows[3].Values.ToArray(), double.Parse);
-
-            matrix = new rtMatrix(row1[0], row1[1], row1[2], row1[3], row2[0], row2[1], row2[2], row2[3], row3[0], row3[1], row3[2], row3[3], row4[0], row4[1], row4[2], row4[3]);
+            matrix = Create4x4MatrixFromTable(table);
         }
         
         [Then(@"m\((.*), (.*)\) == (.*)")]
@@ -54,23 +49,13 @@ namespace RayTracer.Tests.features
         [Given(@"the following matrixA:")]
         public void GivenTheFollowingMatrixA(Table table)
         {
-            double[] row1 = Array.ConvertAll(table.Rows[0].Values.ToArray(), double.Parse);
-            double[] row2 = Array.ConvertAll(table.Rows[1].Values.ToArray(), double.Parse);
-            double[] row3 = Array.ConvertAll(table.Rows[2].Values.ToArray(), double.Parse);
-            double[] row4 = Array.ConvertAll(table.Rows[3].Values.ToArray(), double.Parse);
-
-            matrixA = new rtMatrix(row1[0], row1[1], row1[2], row1[3], row2[0], row2[1], row2[2], row2[3], row3[0], row3[1], row3[2], row3[3], row4[0], row4[1], row4[2], row4[3]);
+            matrixA = Create4x4MatrixFromTable(table);
         }
 
         [Given(@"the following matrixB:")]
         public void GivenTheFollowingMatrixB(Table table)
         {
-            double[] row1 = Array.ConvertAll(table.Rows[0].Values.ToArray(), double.Parse);
-            double[] row2 = Array.ConvertAll(table.Rows[1].Values.ToArray(), double.Parse);
-            double[] row3 = Array.ConvertAll(table.Rows[2].Values.ToArray(), double.Parse);
-            double[] row4 = Array.ConvertAll(table.Rows[3].Values.ToArray(), double.Parse);
-
-            matrixB = new rtMatrix(row1[0], row1[1], row1[2], row1[3], row2[0], row2[1], row2[2], row2[3], row3[0], row3[1], row3[2], row3[3], row4[0], row4[1], row4[2], row4[3]);
+            matrixB = Create4x4MatrixFromTable(table);
         }
 
         [Then(@"matrixA == matrixB")]
@@ -88,16 +73,26 @@ namespace RayTracer.Tests.features
         [Then(@"matrixA \* matrixB is the follwing matrix:")]
         public void ThenMatrixAMatrixBIsTheFollwingMatrix(Table table)
         {
-            double[] row1 = Array.ConvertAll(table.Rows[0].Values.ToArray(), double.Parse);
-            double[] row2 = Array.ConvertAll(table.Rows[1].Values.ToArray(), double.Parse);
-            double[] row3 = Array.ConvertAll(table.Rows[2].Values.ToArray(), double.Parse);
-            double[] row4 = Array.ConvertAll(table.Rows[3].Values.ToArray(), double.Parse);
-
-            var expected = new rtMatrix(row1[0], row1[1], row1[2], row1[3], row2[0], row2[1], row2[2], row2[3], row3[0], row3[1], row3[2], row3[3], row4[0], row4[1], row4[2], row4[3]);
+            var expected = Create4x4MatrixFromTable(table);
 
             rtMatrix result = matrixA * matrixB;
             Assert.AreEqual(expected, result);
         }
 
+        /// <summary>
+        /// Helper method to create a matrix from a SpecFlow table
+        /// </summary>
+        /// <param name="table"></param>
+        /// <returns></returns>
+        private rtMatrix Create4x4MatrixFromTable(Table table)
+        {
+            double[] row1 = Array.ConvertAll(table.Rows[0].Values.ToArray(), double.Parse);
+            double[] row2 = Array.ConvertAll(table.Rows[1].Values.ToArray(), double.Parse);
+            double[] row3 = Array.ConvertAll(table.Rows[2].Values.ToArray(), double.Parse);
+            double[] row4 = Array.ConvertAll(table.Rows[3].Values.ToArray(), double.Parse);
+
+            var matrix = new rtMatrix(row1[0], row1[1], row1[2], row1[3], row2[0], row2[1], row2[2], row2[3], row3[0], row3[1], row3[2], row3[3], row4[0], row4[1], row4[2], row4[3]);
+            return matrix;
+        }
     }
 }
